d707fd4041887f600d069fab6db95219539ef2ca
Alexander Musyoki <amusyoki.gm@gmail.com>
2022-04-01 21:41:59 +0300

    Fixed Bugs

--- js/main.js
+++ js/main.js
@@ -1,45 +1,4 @@
-$(function() {
-
-	'use strict';
-
-	// Form
-
-	var contactForm = function() {
-
-		if ($('#contactForm').length > 0 ) {
-			$( "#contactForm" ).validate( {
-				rules: {
-					name: "required",
-					firstName:{
-						required: true,
-						firstName: true,
-
-					},
-					email: {
-						required: true,
-						email: true
-					},
-					message: {
-						required: true,
-						minlength: 5
-					}
-				},
-				messages: {
-					name: "Please enter your name",
-					firstNbame: "Please enter your name",
-					email: "Please enter a valid email address",
-					message: "Please enter a message"
-				},
-			} );
-		}
-	};
-
-});
-
-
 //Validity Check
 const entries = document.getElementsByTagName("input");
 const button = document.getElementById("next")
-button.addEventListener("click", function(){
-	entries.every(input => input.reportValidity());
-})
+const inputs = entries.every(input => input.reportValidity());
