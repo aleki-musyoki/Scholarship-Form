78312e35619adfebfaf320388d6112afe0916fd3
Alexander Musyoki <amusyoki.gm@gmail.com>
2022-04-01 23:02:15 +0300

    Fixed Button Issues

--- js/main.js
+++
@@ -1,4 +0,0 @@
-//Validity Check
-const entries = document.getElementsByTagName("input");
-const button = document.getElementById("next")
-const inputs = entries.every(input => input.reportValidity());
